5e128773c322157e0eb464918ab2aa7ee4787579
unknown <eslamyounis542@yahoo.com>
2021-03-25 03:54:56 +0200

    before 10 deploy

--- src/app/Services/profile.service.ts
+++ src/app/Services/profile.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 
-import { HttpClient , HttpClientModule, HttpHeaders } from '@angular/common/http';
+import { HttpClient } from '@angular/common/http';
 import { Subject } from 'rxjs';
 
 
